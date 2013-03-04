48e6228554efb5494aea85abcd438a5b402746a5
fitark <fitark@fitark-virtual-machine.(none)>
2013-03-04 12:04:35 +0800

    初始化仓库

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'e76b628a9ab41161cbc0fd3c7b147f4bce403607264fe34a1b500f4bfa07428b41d714e68d8f00c3236fcd8965a08422ae7a95eab034156a7004ef6c767ab5fe'
